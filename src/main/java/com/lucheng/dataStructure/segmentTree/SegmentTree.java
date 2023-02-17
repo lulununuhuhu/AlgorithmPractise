e60f1f3935291dc96cd81ebca1b473f417b33e15
Lucheng <1078496642@qq.com>
2023-02-17 15:18:18 +0800

    使用线段树和二分查找完成最大公约数问题

--- src/main/java/com/lucheng/dataStructure/segmentTree/SegmentTree.java
+++ src/main/java/com/lucheng/dataStructure/segmentTree/SegmentTree.java
@@ -5,12 +5,20 @@ package com.lucheng.dataStructure.segmentTree;
  */
 public class SegmentTree {
     private static int[] array = new int[]{1,5,6,9,8,7,8,6,3};
-    private static int[] segmentTree = new int[array.length*2];
+    private static int[] segmentTree = new int[array.length*4];
+    
+    private static int[] mark = new int[array.length*4];
 
     public static void main(String[] args) {
         build(0, array.length-1, 1);
         for (int i : segmentTree) {
             System.out.print(i + " ");
+        }
+
+        update(2,5,2,1,0,8);
+        System.out.println("更新后的线段树内容:");
+        for (int i : segmentTree) {
+            System.out.print(i+" ");
         }
     }
 
@@ -33,6 +41,39 @@ public class SegmentTree {
             build(mid+1,r,p*2+1);
             //segmentTree[p] 的值是左右节点的值的和
             segmentTree[p] = segmentTree[p*2]+segmentTree[p*2+1];
+        }
+    }
+
+    /**
+     * 在目标区间[l,r]中的每个数加上d之后更新线段树内容
+     * @param l
+     * @param r
+     * @param d 增加值
+     * @param p 线段树序号
+     * @param cl 当前左区间
+     * @param cr 当前右区间
+     */
+    private static void update(int l, int r, int d, int p , int cl , int cr )
+    {
+        if (cl > r || cr < l) // 区间无交集
+            return; // 剪枝
+        else if (cl >= l && cr <= r) // 当前节点对应的区间包含在目标区间中
+        {
+            segmentTree[p] += (cr - cl + 1) * d; // 更新当前区间的值
+            if (cr > cl) // 如果不是叶子节点
+                mark[p] += d; // 给当前区间打上标记
+        }
+        else // 与目标区间有交集，但不包含于其中
+        {
+            int mid = (cl + cr) / 2;
+            mark[p * 2] += mark[p]; // 标记向下传递
+            mark[p * 2 + 1] += mark[p];
+            segmentTree[p * 2] += mark[p] * (mid - cl + 1); // 往下更新一层
+            segmentTree[p * 2 + 1] += mark[p] * (cr - mid);
+            mark[p] = 0; // 清除标记
+            update(l, r, d, p * 2, cl, mid); // 递归地往下寻找
+            update(l, r, d, p * 2 + 1, mid + 1, cr);
+            segmentTree[p] = segmentTree[p * 2] + segmentTree[p * 2 + 1]; // 根据子节点更新当前节点的值
         }
     }
 }
--- src/main/java/com/lucheng/lanqiaobei/最大公约数.java
+++ src/main/java/com/lucheng/lanqiaobei/最大公约数.java
@@ -4,12 +4,58 @@ package com.lucheng.lanqiaobei;
  * 题目链接：https://www.dotcpp.com/oj/problem2709.html
  */
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  * 求最少操作次数就是求得获取某两个相邻数得最大公约数为1的最少操作次数
  */
 public class 最大公约数 {
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    static int N = 100010;
+    static Node[] tr = new Node[N * 4];//构建线段树的长度一般为对应数组长度的4倍
+    static int[] a = new int[N];
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int f = 0;//记录数组中出现1的个数
+        for (int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
+            if(a[i] == 1)   f++;
+        }
 
+        if(f != 0){
+            //数组中有1
+            out.println(n-f);//输出结果就是数组中不是1的个数
+            out.flush();
+            return;
+        }
+
+        //构建线段树
+        build(1,n,1);
+
+        //如果整个区间的最大gcd不是1，那就说明任何一个子数组的最大gcd也不为1
+        if(query(1,n,1) != 1){
+            out.println(-1);
+            out.flush();
+            return;
+        }
+
+        //找到长度最短的子数组满足gcd为1
+        int ans = 0x7fffffff;//维护gcd为1的子数组的最短距离
+        for (int i = 1; i <= n; i++) {
+            int l = i+1,r = n+1;//查询区间[i+1,n+1)
+            while (l < r){
+                int mid = l + r >> 1;
+                if(query(i,mid,1) == 1) r = mid;//r及r右侧区间的最大gcd都为1
+                else l = mid+1;//l左侧区间的最大gcd都不为1
+            }
+            if(query(i,r,1) == 1) ans = Math.min(ans,r-i);
+        }
+
+        out.println(n-1+ans);//最小操作次数等于数组长度+gcd为1的最短子数组的长度-1
+        out.flush();
     }
 
     /**
@@ -18,7 +64,71 @@ public class 最大公约数 {
      * @param y
      * @return
      */
-    private int gcd(int x,int y){
+    private static int gcd(int x,int y){
         return y == 0 ? x:gcd(y,x%y);
+    }
+
+    /**
+     * 构建线段树
+     * @param l
+     * @param r
+     * @param p
+     */
+    static void build(int l,int r,int p){
+        if(l == r){
+            //到达子节点
+            tr[p] = new Node(l,r,a[r]);
+        }else{
+            tr[p] = new Node(l,r,0);
+            //构建子节点
+            int mid = (l+r)/2;
+            //左子节点的区间是[l,mid]
+            build(l,mid,p*2);
+            //右子节点的区间是[mid+1,r]
+            build(mid+1,r,p*2+1);
+            pushup(p);
+        }
+    }
+
+    /**
+     * 线段树节点的值存放的是该节点所对应区间的最大公约数gcd
+     * @param p
+     */
+    static void pushup(int p) {
+        tr[p].g = gcd(tr[p*2].g,tr[p*2+1].g);
+    }
+
+    /**
+     * 查询区间[l,r]的最大gcd
+     * @param l
+     * @param r
+     * @param p
+     * @return
+     */
+    static int query(int l,int r,int p){
+        if(tr[p].l>=l && tr[p].r<=r) return tr[p].g;//tr[p]表示的区间是[l,r]的子区间
+        int mid = tr[p].l+tr[p].r >> 1;
+        if(r <= mid)    return query(l,r,p<<1);//[l,r]是p号节点的左儿子节点的子区间
+        else if (l > mid) {
+            return query(l,r,p << 1 | 1);//[l,r]是在p号节点的右儿子节点的子区间
+        }else {
+            //[l,r]在p节点的两个区间都有交集
+            return gcd(query(l,r,p << 1),query(l,r,p << 1 | 1));
+        }
+    }
+
+    /**
+     * 线段树节点的数据结构
+     * l:左区间
+     * r:右区间
+     * g:[l,r]中的gcd
+     */
+    static class Node{
+        int l,r,g;
+        public Node(int l, int r, int g) {
+            this.l = l;
+            this.r = r;
+            this.g = g;
+        }
     }
 }
